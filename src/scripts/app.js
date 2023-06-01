fec42e7cb85bac7f4a4a2d98b96d5113a348dee2
Jordan Topp <toppdat@gmail.com>
2023-06-01 16:44:17 -0700

    Added mobile navigation

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,11 @@
 // Update copyright year to current year
 const copyYear = document.querySelector('#copyright-year');
 copyYear.innerHTML = new Date().getFullYear();
+
+//Mobile Navigation
+const navButton = document.querySelector('.hamburger')
+const header = document.querySelector('.header')
+
+navButton.addEventListener('click', () => {
+    header.classList.toggle('nav-open')
+})
